f7249cd2ff668f2b54be874597594d1a5e278dc6
Bibi Aasiyaa Jaulim <60102393@udst.edu.qa>
2024-02-01 16:14:20 +0300

    Third commit, modelYear() function added

--- cars.py
+++ cars.py
@@ -1,5 +1,11 @@
 import random
 
+def modelYear():
+    '''returns a model year'''
+    year = [2021, 2022, 2023, 2024]
+    return random.choice(year)
+
 print("This is file cars.py")
 cars = ["Mercedes-Benz","Audi","Lexus","BMW"]
 print("Car:",random.choice(cars))
+print("Model year:",modelYear())
